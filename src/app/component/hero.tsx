eb609de64220ebf0658a3befe0b70dc35bc7b674
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2024-10-09 22:25:31 +0500

    Next.js Portfolio

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { TypeAnimation } from 'react-type-animation'
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Hero() {
   return (
